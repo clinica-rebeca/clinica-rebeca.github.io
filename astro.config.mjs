0876e1d943b37a01a03fdd3690b3bd5a32bb317d
Carlos Junquera <carlos@galicloud.com>
2022-11-03 19:01:11 +0100

    github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://carlosgalicloud.github.io',
+  base: '/clinica-rebeca',
   integrations: [
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
